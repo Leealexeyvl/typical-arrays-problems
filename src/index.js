74dc52fe5d676a0396af3161dddc4e54a7ad8c24
leealexeyvl <leealexey_94@mail.ru>
2021-03-12 13:12:12 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
 
+// exports.min = function min (array) {
+//   if (array === undefined || array.length == 0) {
+//     return 0;
+//   };
+//   let result = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < result) {
+//       result = array[i];
+//     }
+//   }
+//   return result;
+// }
+
+// exports.max = function max (array) {
+//   if (array === undefined || array.length == 0) {
+//     return 0;
+//   };
+//   let result = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] > result) {
+//       result = array[i];
+//     }
+//   }
+//   return result;
+// }
+
+// exports.avg = function avg (array) {
+//   if (array === undefined || array.length == 0) {
+//     return 0;
+//   };
+//   let result = array[0];
+//   for (let i = 1; i < array.length; i++) {
+//       result += array[i];
+//   }
+//   return result / array.length;
+// }
 exports.min = function min (array) {
-  if (array === undefined || array.length == 0) {
-    return 0;
-  };
-  let result = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < result) {
-      result = array[i];
-    }
-  }
-  return result;
+  return 0;
 }
 
 exports.max = function max (array) {
-  if (array === undefined || array.length == 0) {
-    return 0;
-  };
-  let result = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > result) {
-      result = array[i];
-    }
-  }
-  return result;
+  return 0;
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined || array.length == 0) {
-    return 0;
-  };
-  let result = array[0];
-  for (let i = 1; i < array.length; i++) {
-      result += array[i];
-  }
-  return result / array.length;
+  return 0;
 }
